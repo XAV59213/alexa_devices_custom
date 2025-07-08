c38d55e8f730aa697b713ebca976ac5951a4dd3a
XAV59213 <153436936+XAV59213@users.noreply.github.com>
2025-07-08 18:48:22 +0200

    Update __init__.py

--- custom_components/alexa_devices_custom/__init__.py
+++ custom_components/alexa_devices_custom/__init__.py
@@ -1,4 +1,4 @@
-"""Alexa Devices integration."""
+"""Alexa Devices Custom integration."""
 
 from homeassistant.const import Platform
 from homeassistant.core import HomeAssistant
@@ -12,14 +12,18 @@ PLATFORMS = [
     Platform.SWITCH,
 ]
 
+
 async def async_setup_entry(hass: HomeAssistant, entry: AmazonConfigEntry) -> bool:
+    """Set up Alexa Devices Custom from a config entry."""
     coordinator = AmazonDevicesCoordinator(hass, entry)
     await coordinator.async_config_entry_first_refresh()
     entry.runtime_data = coordinator
     await hass.config_entries.async_forward_entry_setups(entry, PLATFORMS)
     return True
 
+
 async def async_unload_entry(hass: HomeAssistant, entry: AmazonConfigEntry) -> bool:
+    """Unload Alexa Devices Custom config entry."""
     coordinator = entry.runtime_data
     if unload_ok := await hass.config_entries.async_unload_platforms(entry, PLATFORMS):
         await coordinator.api.close()
